eaaed82c2d5a810b107d70e470a3e513e5bbf34c
WonderJ13 <wonderj13@gmail.com>
2020-12-04 00:52:38 -0600

    day4 part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::BufRead;
+use std::str::FromStr;
 
 #[derive(Debug)]
 struct Passport {
@@ -14,8 +15,95 @@ struct Passport {
 }
 
 fn validate_passport(p: Passport) -> bool {
-    return p.byr != "" && p.iyr != "" && p.eyr != "" && p.hgt != "" &&
-            p.hcl != "" && p.ecl != "" && p.pid != ""
+    //BYR
+    let byr = i32::from_str(p.byr.as_str());
+    let byr = match byr {
+        Ok(b) => b,
+        Err(_) => 0,
+    };
+    if byr < 1920 || byr > 2002 {
+        return false;
+    }
+
+    //IYR
+    let iyr = i32::from_str(p.iyr.as_str());
+    let iyr = match iyr {
+        Ok(i) => i,
+        Err(_) => 0,
+    };
+    if iyr < 2010 || iyr > 2020 {
+        return false;
+    }
+
+    //EYR
+    let eyr = i32::from_str(p.eyr.as_str());
+    let eyr = match eyr {
+        Ok(e) => e,
+        Err(_) => 0,
+    };
+    if eyr < 2020 || eyr > 2030 {
+        return false;
+    }
+
+    //HGT
+    if p.hgt.ends_with("in") {
+        let hgt = i32::from_str(p.hgt.strip_suffix("in").unwrap());
+        let hgt = match hgt {
+            Ok(h) => h,
+            Err(_) => 0,
+        };
+        if hgt < 59 || hgt > 76 {
+            return false;
+        }
+    } else if p.hgt.ends_with("cm") {
+        let hgt = i32::from_str(p.hgt.strip_suffix("cm").unwrap());
+        let hgt = match hgt {
+            Ok(h) => h,
+            Err(_) => 0,
+        };
+        if hgt < 150 || hgt > 193 {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    //HCL
+    if p.hcl.len() != 7 || !p.hcl.starts_with("#") {
+        return false;
+    }
+    let hcl = i32::from_str_radix(p.hcl.strip_prefix("#").unwrap(), 16);
+    let hcl = match hcl {
+        Ok(h) => h,
+        Err(_) => -1,
+    };
+    if hcl < 0 || hcl > 0xFFFFFF {
+        return false;
+    }
+
+    //ECL
+    let ecl = match p.ecl.as_str() {
+        "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+        _ => false,
+    };
+    if !ecl {
+        return false;
+    }
+
+    //PID
+    if p.pid.len() != 9 {
+        return false;
+    }
+    let pid = i32::from_str(p.pid.as_str());
+    let pid = match pid {
+        Ok(p) => p,
+        Err(_) => -1,
+    };
+    if pid < 0 || pid > 999_999_999 {
+        return false;
+    }
+
+    return true;
 }
 
 fn main() {
