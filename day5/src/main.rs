15b146cabdfe19946db9ba85fe68b8377192278b
WonderJ13 <wonderj13@gmail.com>
2020-12-05 18:35:08 -0600

    day5 part2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -14,12 +14,16 @@ fn main() {
     let stdin = io::stdin();
     let stdin = stdin.lock();
 
-    let mut max_id: i32 = -1;
+    let mut id_filled: [bool; 1024] = [false; 1024];
     for line in stdin.lines() {
-        let id = id_from_pass(line.unwrap());
-        if max_id < id {
-            max_id = id;
+        id_filled[id_from_pass(line.unwrap()) as usize] = true
+    }
+    //first viable seat has to be id 9
+    //last viable seat has to be id 1015
+    for i in 9..1015 {
+        if !id_filled[i] {
+            println!("{}", i);
+            break;
         }
     }
-    println!("{}", max_id);
 }
