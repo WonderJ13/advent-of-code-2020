8b0d09999e4e2585a0856ed49b2cd9653ad16e5f
WonderJ13 <wonderj13@gmail.com>
2020-12-05 23:47:51 -0600

    day6 part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,18 +1,19 @@
 use std::io;
 use std::io::BufRead;
 
-fn count_group(answers: Vec<String>) -> u8 {
-    let mut answer_sheet: [bool; 26] = [false; 26];
-    for answer in answers {
-        for a in answer.chars() {
+fn count_group(passenger_answers: Vec<String>) -> u8 {
+    let mut answer_sheet: [u64; 26] = [0; 26];
+    let passenger_count: u64 = passenger_answers.len() as u64;
+    for passenger in passenger_answers {
+        for a in passenger.chars() {
             let i = (a as usize) - ('a' as usize);
-            answer_sheet[i] = true;
+            answer_sheet[i] += 1;
         }
     }
 
     let mut count = 0;
-    for a in answer_sheet.iter() {
-        count += *a as u8;
+    for answer_count in answer_sheet.iter() {
+        count += if passenger_count == *answer_count { 1 } else { 0 };
     }
     count
 }
